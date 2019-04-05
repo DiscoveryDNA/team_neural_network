3c63d12409b85768eccd61d821f3f3adfc687d24
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2019-04-05 04:33:49 -0700

    added tf model

--- code/experiments/conv_rnn_hybrid_model/utilities.py
+++ code/experiments/conv_rnn_hybrid_model/utilities.py
@@ -1,8 +1,9 @@
-import os, shutil, pickle, shelve
-from Bio import SeqIO
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
+#import os, shutil, pickle, shelve
+#from Bio import SeqIO
+#import numpy as np
+#import tensorflow as tf
+#import matplotlib.pyplot as plt
+
 
 def sampling(src, dest, num_samples, seed=42):
     """  
@@ -12,15 +13,16 @@ def sampling(src, dest, num_samples, seed=42):
     np.random.seed(seed)
     all_data_lst = np.array(os.listdir(src))
     n = len(all_data_lst)
-    sample_indices = np.random.choice(np.arange(n), num_samples, replace = False)
+    sample_indices = np.random.choice(np.arange(n), num_samples, replace=False)
     sample_files = all_data_lst[sample_indices]
 
     for file in sample_files:
         shutil.copy(os.path.join(src, file),
-                            dest)
+                    dest)
     print('copied samples to {}'.format(dest))
-        
-def one_hot_encoding(input_folder_path, output_file_path, 
+
+
+def one_hot_encoding(input_folder_path, output_file_path,
                      max_file_num=10000):
     """
     Given the data in INPUT_FOLDER_PATH, encode them and save
@@ -31,7 +33,7 @@ def one_hot_encoding(input_folder_path, output_file_path,
     """
     import os
     # Use the following dictionary to perform the transformation
-    base_pairs = {'A': [1, 0, 0, 0], 
+    base_pairs = {'A': [1, 0, 0, 0],
                   'C': [0, 1, 0, 0],
                   'G': [0, 0, 1, 0],
                   'T': [0, 0, 0, 1],
@@ -42,7 +44,7 @@ def one_hot_encoding(input_folder_path, output_file_path,
                   'n': [0, 0, 0, 0],
                   'N': [0, 0, 0, 0]}
 
-    file_num_limit = max_file_num    # The maximum number of files to be decoded
+    file_num_limit = max_file_num  # The maximum number of files to be decoded
     file_count = 0
 
     # Iterate through every file
@@ -50,7 +52,7 @@ def one_hot_encoding(input_folder_path, output_file_path,
     for file in os.listdir(input_folder_path):
         # When the number of file decoded has reached the limit, stop
         if file_count < file_num_limit:
-            data = list(SeqIO.parse(input_folder_path + file,"fasta"))
+            data = list(SeqIO.parse(input_folder_path + file, "fasta"))
             for n in range(0, len(data)):
                 # Extract the header information
                 header = data[n].description.split('|')
@@ -60,10 +62,10 @@ def one_hot_encoding(input_folder_path, output_file_path,
                 speciesID = header[2]
                 strand = header[3]
                 # Complement all sequences in the negative DNA strand
-    #             if strand == '-':
-    #                 # Using the syntax [e for e in base_pairs[n]] to create a new pointer for each position
-    #                 one_hot.append([descr, expressed, speciesID, [[e for e in base_pairs[n]] for n in data[n].seq.complement()]])
-    #             else:
+                #             if strand == '-':
+                #                 # Using the syntax [e for e in base_pairs[n]] to create a new pointer for each position
+                #                 one_hot.append([descr, expressed, speciesID, [[e for e in base_pairs[n]] for n in data[n].seq.complement()]])
+                #             else:
                 all_regions.append([descr, expressed, speciesID, [[e for e in base_pairs[n]] for n in data[n].seq]])
             file_count += 1
 
@@ -71,6 +73,7 @@ def one_hot_encoding(input_folder_path, output_file_path,
         print("save to {}".format(output_file_path))
         pickle.dump(all_regions, output)
     return all_regions
+
 
 def curtail(lst, read_len):
     """ A helper function of get_training_data
@@ -81,6 +84,7 @@ def curtail(lst, read_len):
         for i in range(read_len - len(lst)):
             lst.append([0, 0, 0, 0])
     return lst
+
 
 def get_training_data(input_data, output_folder_path,
                       max_len, train_x_name, train_y_name):
@@ -95,8 +99,8 @@ def get_training_data(input_data, output_folder_path,
     for region in input_data:
         y, x = int(region[1]), region[3]
         x = curtail(x, max_len)  # Curtail
-        x = np.array(x).flatten() # Flatten
-        x = x.reshape((1000, 4)) # Reshape
+        x = np.array(x).flatten()  # Flatten
+        x = x.reshape((1000, 4))  # Reshape
         train_x.append(x)
         train_y.append(y)
 
@@ -113,6 +117,7 @@ def get_training_data(input_data, output_folder_path,
         pickle.dump(train_y, output)
     return train_x, train_y
 
+
 def load_data(data_folder_path, train_x_name, train_y_name):
     output_name = train_x_name
     with open(os.path.join(output_folder_path, output_name), 'rb') as file:
@@ -123,7 +128,8 @@ def load_data(data_folder_path, train_x_name, train_y_name):
         data_y = pickle.load(file)
 
     print(data_x.shape, data_y.shape)
-    
+
+
 def data_split(data_x, data_y, val_split=0.2, seed=42):
     """
     Given totally N data, randomly sample N*VAL_SPLIT of them 
@@ -133,16 +139,17 @@ def data_split(data_x, data_y, val_split=0.2, seed=42):
     # Split it into training and validation data sets
     N = data_x.shape[0]
     num_val = int(N * val_split)
-    
+
     np.random.seed(seed)
     val_indices = np.random.choice(np.arange(N), num_val, replace=False)
     train_indices = np.arange(N)[~np.isin(np.arange(N), val_indices)]
 
     train_x, train_y = data_x[train_indices], data_y[train_indices]
     val_x, val_y = data_x[val_indices], data_y[val_indices]
-    
+
     print(N, train_x.shape, train_y.shape, val_x.shape, val_y.shape)
     return train_x, train_y, val_x, val_y
+
 
 def dianostic_plots(train_acc, train_loss, val_train_acc, val_loss):
     """  Plot dianostic plots of a model:
@@ -150,7 +157,7 @@ def dianostic_plots(train_acc, train_loss, val_train_acc, val_loss):
     Plot 2: Training acc & validation acc against epochs
     """
     epochs = range(1, len(train_acc) + 1)
-
+    
     plt.plot(epochs, train_acc, '-', label='Training train_accuracy')
     plt.plot(epochs, val_train_acc, '-', label='Validation Accuracy')
     plt.title('Training and Validation Accuracy')
@@ -163,5 +170,5 @@ def dianostic_plots(train_acc, train_loss, val_train_acc, val_loss):
     plt.title('Training and Validation Loss')
     plt.xlabel('epoches')
     plt.legend()
-    
+
     plt.show()
